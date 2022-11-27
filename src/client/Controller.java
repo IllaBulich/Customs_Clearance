dd4fb98b7c5e94c5381aca76f61a180013d76d9e
IllaBulich <mba.2004@mail.ru>
2022-11-27 23:10:04 +0300

    простая прога готова

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,25 +1,35 @@
 package client;
 
-import java.io.IOException;
-import java.net.URL;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.ResourceBundle;
-
 import client.resources.Сalculations;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
 import org.xml.sax.SAXException;
 
-import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
 
 public class Controller {
-
+    Сalculations calc;
+    float well = (float) 0.9688;
+    String[] list = new String[]{
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь",
+            "Декабрь"};
     @FXML
     private Text Info;
 
@@ -81,7 +91,7 @@ public class Controller {
     @FXML
     void getHuman(ActionEvent event) {
 
-        if(getRadioButton1.isSelected()){
+        if (getRadioButton1.isSelected()) {
             getRadioButton3.setVisible(false);
             getRadioButton4.setVisible(false);
             Text.setVisible(false);
@@ -91,60 +101,87 @@ public class Controller {
             Text.setVisible(true);
         }
     }
+
     @FXML
     void initialize() {
-        getComboBox.setItems(FXCollections.observableArrayList("$","€"));
+        getComboBox.setItems(FXCollections.observableArrayList("$", "€"));
         getComboBox.setValue("€");
 
-        getMonth.setItems(FXCollections.observableArrayList(
-                "Январь",
-                "Февраль",
-                "Март",
-                "Апрель",
-                "Май",
-                "Июнь",
-                "Июль",
-                "Август",
-                "Сентябрь",
-                "Октябрь",
-                "Ноябрь",
-                "Декабрь"));
+        getMonth.setItems(FXCollections.observableArrayList(list));
         getMonth.setValue("Январь");
 
         LocalDate date = LocalDate.now();
 
-        for(int i = date.getYear();i>1951;i-- ){
+        for (int i = date.getYear(); i > 1951; i--) {
             getYear.getItems().add(String.valueOf(i));
         }
         getYear.setValue(String.valueOf(date.getYear()));
         getData.setOnAction(actionEvent -> {
             if (isInputValid()) {
                 if (getRadioButton1.isSelected()) {
-                    Сalculations calc;
-                    try {
-                        calc = new Сalculations(
-                                Integer.parseInt(getTextFild2.getText()),
-                                date.getYear()-Integer.parseInt(getYear.getValue()) +
-                                        (float)(date.getMonthValue()-intMonth(getMonth.getValue())) / 12,
-                                Integer.parseInt(getTextFild1.getText()),
-                                getComboBox.getValue()
-                        );
-                    } catch (ParserConfigurationException e) {
-                        throw new RuntimeException(e);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    } catch (SAXException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                    System.out.println(calc.PhysicalCalc());
+                    Physical(date);
                 } else {
-                    int text = Integer.parseInt(getTextFild2.getText());
-                    System.out.println(text);
+                    Legal(date);
 
                 }
             }
         });
+    }
+
+    private void Physical(LocalDate date) {
+        try {
+            calc = new Сalculations(
+                    (getComboBox.getValue() == "$") ? (int) (Integer.parseInt(getTextFild2.getText()) * well) : Integer.parseInt(getTextFild2.getText()),
+                    date.getYear() - Integer.parseInt(getYear.getValue()) + (float) (date.getMonthValue() - intMonth(getMonth.getValue())) / 12,
+                    Integer.parseInt(getTextFild1.getText())
+
+            );
+
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
+        int cost = calc.PhysicalCalc();
+        Info.setVisible(true);
+        Info2.setVisible(false);
+        Info1.setVisible(false);
+        Info.setText("Сумма таможенной пошлины составит: " + cost + " € (" + (int) (cost / well) + " $)");
+    }
+
+    private void Legal(LocalDate date) {
+        int price =(getComboBox.getValue() == "$") ? (int) (Integer.parseInt(getTextFild2.getText()) * well) : Integer.parseInt(getTextFild2.getText());
+        try {
+            calc = new Сalculations(
+                    price,
+                    date.getYear() - Integer.parseInt(getYear.getValue()) + (float) (date.getMonthValue() - intMonth(getMonth.getValue())) / 12,
+                    Integer.parseInt(getTextFild1.getText())
+
+            );
+
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (SAXException e) {
+            throw new RuntimeException(e);
+        }
+        int cost=0;
+        if (getRadioButton3.isSelected()) {
+            cost = calc.LegalPetrol("petrol");
+        } else if (getRadioButton4.isSelected()) {
+            cost = calc.LegalPetrol("diesel");
+        }
+        int NDS = (int) ((cost + price)*0.2);
+        Info.setVisible(true);
+        Info2.setVisible(true);
+        Info1.setVisible(true);
+        Info.setText("Сумма таможенной пошлины составит: " + cost + " € (" + (int) (cost / well) + " $)");
+        Info1.setText("Сумма НДС: " + NDS +" € (" + (int) (NDS / well) + " $)");
+        Info2.setText("Итого: " + (cost+NDS) + " € (" + (int) ((cost+NDS) / well) + " $)");
+
     }
 
     private boolean isInputValid() {
@@ -156,14 +193,18 @@ public class Controller {
         if (getTextFild2.getText() == null || getTextFild2.getText().length() == 0) {
             errorMessage += "Не указана стоимость транспортного средства!\n";
         }
-        if (getComboBox.getValue() == null ) {
+        if (getComboBox.getValue() == null) {
             errorMessage += "Не указана валюта!\n";
         }
 
-        if ( getMonth.getValue() == null || getYear.getValue()  == null ) {
+        if (getMonth.getValue() == null || getYear.getValue() == null) {
             errorMessage += "Не указана дата выпуска!\n";
         }
+        if (getRadioButton2.isSelected()) {
+            if (getRadioButton3.isSelected() || getRadioButton4.isSelected()) {
+            } else errorMessage += "1Не указана тип двигателя!\n";
 
+        }
         if (errorMessage.length() == 0) {
             return true;
         } else {
@@ -178,23 +219,12 @@ public class Controller {
             return false;
         }
     }
-    private int intMonth(String month){
-        String[] list = new String[] {
-                "Январь",
-                "Февраль",
-                "Март",
-                "Апрель",
-                "Май",
-                "Июнь",
-                "Июль",
-                "Август",
-                "Сентябрь",
-                "Октябрь",
-                "Ноябрь",
-                "Декабрь"};
-        for (int i=0;i<12;i++)
-            if (month==list[i])
-                return i+1;
+
+    private int intMonth(String month) {
+
+        for (int i = 0; i < 12; i++)
+            if (month == list[i])
+                return i + 1;
         return 1;
     }
 
--- src/client/resources/Сalculations.java
+++ src/client/resources/Сalculations.java
@@ -18,14 +18,14 @@ public class Сalculations {
     private int cost;
     private float age;
     private int volume;
-    private String currency;
+
     private NodeList employeeElements;
 
-    public Сalculations(int cost,float age, int volume, String currency) throws ParserConfigurationException, IOException, SAXException {
+    public Сalculations(int cost, float age, int volume) throws ParserConfigurationException, IOException, SAXException {
         this.cost = cost;
         this.age = age;
         this.volume = volume;
-        this.currency = currency;
+
         // Получение фабрики, чтобы после получить билдер документов.
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
@@ -36,58 +36,82 @@ public class Сalculations {
         Document document = builder.parse(new File("src/client/duties.xml"));
 
         // Получение списка всех элементов employee внутри корневого элемента (getDocumentElement возвращает ROOT элемент XML файла).
-        this.employeeElements = document.getDocumentElement().getElementsByTagName("period");
+        this.employeeElements = document.getDocumentElement().getElementsByTagName("table");
     }
 
-    public int PhysicalCalc(){
-        // Перебор всех элементов employee
+    public int PhysicalCalc() {
+        // Перебор всех элементов table
         for (int i = 0; i < employeeElements.getLength(); i++) {
             Node employee = employeeElements.item(i);
 
             // Получение атрибутов каждого элемента
             NamedNodeMap attributesYaer = employee.getAttributes();
 
-
             NodeList attributesParent = employee.getChildNodes();
 
-            System.out.println( attributesYaer.getNamedItem("yearsMax").getNodeValue());
 
-            if (Logics(attributesYaer,"yearsMin","yearsMax", age))
+            if (Logics(attributesYaer, "yearsMin", "yearsMax", age) && Objects.equals(attributesYaer.getNamedItem("engine").getNodeValue(), "null")) {
 
                 for (int j = 0; j < attributesParent.getLength(); j++) {
                     if (!attributesParent.item(j).getNodeName().equals("#text")) {
                         NamedNodeMap attributes = attributesParent.item(j).getAttributes();
 
-                        if (!Objects.equals(attributes.getNamedItem("volumeMin").getNodeValue(), "null")){
-                            if (Logics(attributes,"volumeMin","volumeMax" , volume)){
+                        if (!Objects.equals(attributes.getNamedItem("volumeMin").getNodeValue(), "null")) {
+                            if (Logics(attributes, "volumeMin", "volumeMax", volume)) {
                                 return (int) (volume * Float.parseFloat(attributes.getNamedItem("bid").getNodeValue()));
                             }
-                        }
-                        else {
-                            if (Logics(attributes,"priceMin","priceMax",cost)){
+                        } else {
+                            if (Logics(attributes, "priceMin", "priceMax", cost)) {
                                 return (int) Math.max(cost * Float.parseFloat(attributes.getNamedItem("fromCost").getNodeValue()),
                                         volume * Float.parseFloat(attributes.getNamedItem("bid").getNodeValue()));
                             }
                         }
 
                     }
-
                 }
-
+            }
         }
-
-
         return 0;
     }
 
-    private boolean Logics(NamedNodeMap attr ,String attributesMin,String attributesMax, float x){
-        if (Integer.parseInt(attr.getNamedItem(attributesMin).getNodeValue()) < x &&
-                Objects.equals(attr.getNamedItem(attributesMax).getNodeValue(), "null") ||
-                x <= Integer.parseInt(attr.getNamedItem(attributesMax).getNodeValue()))
-            return true;
-        else return false;
+    public int LegalPetrol( String engineType) {
+// Перебор всех элементов table
+        for (int i = 0; i < employeeElements.getLength(); i++) {
+            Node employee = employeeElements.item(i);
+
+            // Получение атрибутов каждого элемента
+            NamedNodeMap attributesYaer = employee.getAttributes();
+
+            NodeList attributesParent = employee.getChildNodes();
+
+            if (Logics(attributesYaer, "yearsMin", "yearsMax", age)&& Objects.equals(attributesYaer.getNamedItem("engine").getNodeValue(), engineType)){
+
+                for (int j = 0; j < attributesParent.getLength(); j++) {
+                    if (!attributesParent.item(j).getNodeName().equals("#text")) {
+                        NamedNodeMap attributes = attributesParent.item(j).getAttributes();
+
+                        if (Logics(attributes, "volumeMin", "volumeMax", cost)) {
+                            return (int) Math.max(!Objects.equals(attributes.getNamedItem("fromCost").getNodeValue(),"null") ? cost * Float.parseFloat(attributes.getNamedItem("fromCost").getNodeValue()):0,
+                                    volume * Float.parseFloat(attributes.getNamedItem("bid").getNodeValue()));
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
     }
 
+    private boolean Logics(NamedNodeMap attr, String attributesMin, String attributesMax, float x) {
+        if (Objects.equals(attr.getNamedItem(attributesMax).getNodeValue(), "null")) {
+            if (Integer.parseInt(attr.getNamedItem(attributesMin).getNodeValue()) < x)
+                return true;
+        }else {
+            if (Integer.parseInt(attr.getNamedItem(attributesMin).getNodeValue()) < x &&
+                    x <= Integer.parseInt(attr.getNamedItem(attributesMax).getNodeValue()))
+                return true;
+        }
+        return false;
+    }
 
 
 }
